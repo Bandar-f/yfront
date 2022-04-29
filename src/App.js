bd5a512980787474894321948516b10a18215b6b
a <bandarfahdd@gmail.com>
2022-04-30 01:51:29 +0300

    deploy2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react'
+import axios from 'axios'
+
 
 function App() {
+
+  const[data,setData]=useState({})
+
+
+
+  const handleOnChange= e=>{
+
+
+    setData({
+      [e.target.name]:e.target.value,
+  
+    })
+  
+  }
+
+
+  const handleClick= ()=>{
+
+
+  
+  
+    axios.post("http://127.0.0.1:5000/",data,{
+      responseType:"blob",
+    }).then(res=>{
+
+
+    const url = window.URL.createObjectURL(
+
+        new Blob([res.data],{type:res.headers['content-type']})
+        
+        );
+
+
+    const link=document.createElement('a');
+    link.href= url;
+    link.setAttribute('download',"video.mp4");
+    document.body.appendChild(link);
+    link.click();
+
+
+
+
+
+    }).catch()
+  
+  
+  }
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+        
+
+
+        <div className="form-grouph">
+              <label htmlFor="url">please paste the URL </label>
+              <input onChange={handleOnChange}  type="text" name="url" placeholder="ticker"/>
+                </div>
+  
+  
+                
+  
+              
+  
+  
+       <div className="footerh">
+          <button onClick={handleClick} className="btnh" type="button">
+              Download          
+          
+          </button>
+        </div>   
+  
+      
     </div>
   );
 }
